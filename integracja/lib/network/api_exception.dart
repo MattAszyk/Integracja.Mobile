8ac0eec7176d5e9fc59c622bb504f3d7d9e3ea43
Maciej Aszyk <41071101+MattAszyk@users.noreply.github.com>
2021-02-24 21:04:19 +0100

    Update api_exception.dart

---
+++ integracja/lib/network/api_exception.dart
@@ -0,0 +1,22 @@
+class ApiException implements Exception {
+  final String _message;
+  final int _codeStatus;
+  ApiException([this._codeStatus, this._message]);
+
+  @override
+  String toString() {
+    return "$_codeStatus: $_message";
+  }
+}
+
+class BadRequestException extends ApiException {
+  BadRequestException() : super(400, "Bad request");
+}
+
+class UnauthorizedException extends ApiException {
+  UnauthorizedException() : super(401, "Unauthorized");
+}
+
+class UserLogoutException extends ApiException {
+  UserLogoutException() : super(403, "User logout by server.");
+}
