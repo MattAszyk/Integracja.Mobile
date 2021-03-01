b47c22f9b45e593882b774db86729006ea626dd9
Jakub Fladzinski <fladzio321@gmail.com>
2021-03-01 17:10:50 +0100

    Game details screen edit, app bar

--- integracja/lib/pages/game_details/game_details.dart
+++ integracja/lib/pages/game_details/game_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:integracja/models/game.dart';
 import 'package:flutter/rendering.dart';
+import 'package:integracja/utils/constrains.dart';
 
 class BackgroundShape extends CustomClipper<Path> {
   @override
@@ -49,252 +50,234 @@ class GameDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Color.fromARGB(255, 30, 30, 30),
-        body: Column(
-          children: [
-            Stack(
-              children: [
-                ClipPath(
-                  clipper: BackgroundShape(),
-                  child: Container(
-                    height: 240,
-                    color: Color.fromARGB(255, 255, 221, 52),
-                  ),
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 30, 30, 30),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+              ),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(
+                  Icons.logout,
+                  color: Colors.black,
                 ),
-                GestureDetector(
-                  child: ClipPath(
-                    clipper: ButtonShape(),
-                    child: Container(
-                      height: 241,
-                      color: Colors.black,
-                    ),
-                  ),
-                  onTap: () => Navigator.of(context).pop(),
-                ),
-                Positioned(
-                  top: 10,
-                  left: 10,
-                  child: IconButton(
-                      icon: Icon(
-                        Icons.arrow_back_ios,
-                        size: 25,
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      }),
-                ),
-                Positioned(
-                  top: 10,
-                  right: 10,
-                  child: IconButton(
-                      icon: Icon(
-                        Icons.logout,
-                        size: 25,
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      }),
-                ),
-                Container(
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+            centerTitle: true,
+            floating: true,
+            pinned: true,
+            expandedHeight: 180,
+            title: Text('inteGRAcja z zespołem IT'),
+            flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.parallax,
+              background: Container(
+                child: Container(
                   margin: EdgeInsets.only(
-                    top: 70,
+                    top: 100,
                   ),
                   child: Center(
                     child: Column(
                       children: [
-                        Text(
-                          "inteGRAcja z zespołem IT",
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
+                        OutlineButton(
+                            color: Colors.red,
+                            onPressed: () => Navigator.of(context).pop(),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Text(
+                                'GRAJ',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 30,
+                                ),
+                              ),
+                            ),
+                            borderSide: BorderSide(
+                              color: Colors.black, //Color of the border
+                              style: BorderStyle.solid, //Style of the border
+                              width: 1, //width of the border
+                            ),
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(30.0))),
                         SizedBox(
                           height: 10,
                         ),
                         Text(
                           "Startujemy za 30min",
                           style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 45,
-                        ),
-                        Text(
-                          "GRAJ",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                            fontSize: 18,
                           ),
                         ),
                       ],
                     ),
                   ),
                 ),
-              ],
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Ustawienia gry",
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "Początek: 23.02.2021 16:44",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        "Koniec: 23.02.2021 16:44",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        "Maksymalna liczba graczy: 10",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        "Ilość pytań: 20",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "Ustawienia trybu",
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "Nazwa trybu: Szybki",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        "Czas na cały quiz: 10 minut",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        "Ilość żyć: 3",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        "Ilość pytań: 20",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      ExpansionTile(
-                        tilePadding: const EdgeInsets.all(0),
-                        title: Text(
-                          "Gracze",
-                          style: TextStyle(
-                            fontSize: 22,
-                            color: Colors.white,
-                          ),
-                        ),
-                        subtitle: Text(
-                          "6/10",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        children: <Widget>[
-                          Text(
-                            "Agnieszka Uznańska",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            "Maciej Aszyk",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            "Jakub Fladziński",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            "Michał Guźlewski",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            "Mikołaj Suchodolski",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            "Michał Wiciński",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                    ],
-                  ),
-                ),
               ),
             ),
-          ],
-        ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Ustawienia gry",
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Początek: 23.02.2021 16:44",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    "Koniec: 23.02.2021 16:44",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    "Maksymalna liczba graczy: 10",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    "Ilość pytań: 20",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Text(
+                    "Ustawienia trybu",
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Nazwa trybu: Szybki",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    "Czas na cały quiz: 10 minut",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    "Ilość żyć: 3",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    "Ilość pytań: 20",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ExpansionTile(
+                    tilePadding: const EdgeInsets.all(0),
+                    title: Text(
+                      "Gracze",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "6/10",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    children: <Widget>[
+                      Text(
+                        "Agnieszka Uznańska",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "Maciej Aszyk",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "Jakub Fladziński",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "Michał Guźlewski",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "Mikołaj Suchodolski",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "Michał Wiciński",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
