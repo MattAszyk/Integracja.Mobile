f58e8d8484e9c524f01a6c10011c501204478f8b
Jakub Fladzinski <fladzio321@gmail.com>
2021-04-16 19:48:15 +0200

    button edit

--- integracja/lib/pages/game_history/game_history.dart
+++ integracja/lib/pages/game_history/game_history.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:integracja/controllers/game_history/game_history_controller.dart';
 import 'package:integracja/controllers/game_history/game_history_state.dart';
+import 'package:integracja/models/game/game_user_questions.dart';
 import 'package:integracja/models/game/history.dart';
 import 'package:integracja/pages/common/logo.dart';
 import 'package:integracja/pages/game_history/your_score.dart';
@@ -61,23 +62,59 @@ class GameHistoryBody extends StatelessWidget {
         child: Column(
           children: [
             Align(
-                alignment: Alignment.centerLeft,
-                child: YourScore(history: _history)),
-            ExpansionTile(
-              tilePadding: const EdgeInsets.all(0.0),
-              initiallyExpanded: true,
-              title: Text(
-                "Odpowiedzi",
-                style: TextStyle(
-                  fontSize: textBigSize,
-                  color: Colors.white,
-                ),
-              ),
-              children: <Widget>[],
+              alignment: Alignment.centerLeft,
+              child: YourScore(history: _history),
             ),
+            YourAnswers(gameUserQuestions: _history.gameUserQuestions),
           ],
         ),
       ),
+    );
+  }
+}
+
+class YourAnswers extends StatelessWidget {
+  const YourAnswers({
+    Key key,
+    @required List<GameUserQuestions> gameUserQuestions,
+  })  : _gameUserQuestions = gameUserQuestions,
+        super(key: key);
+
+  final List<GameUserQuestions> _gameUserQuestions;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      tilePadding: const EdgeInsets.all(0.0),
+      initiallyExpanded: true,
+      title: Text(
+        "Odpowiedzi",
+        style: TextStyle(
+          fontSize: textBigSize,
+          color: Colors.white,
+        ),
+      ),
+      children: <Widget>[
+        for (var question in _gameUserQuestions)
+          Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  question.question.content,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: textDefaultSize),
+                ),
+              ),
+              for (var answer in question.question.answers)
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(answer.content),
+                ),
+              SizedBox(height: 10.0),
+            ],
+          ),
+      ],
     );
   }
 }
